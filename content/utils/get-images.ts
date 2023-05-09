64cecc5f73a51781b704928d10a8cec1abf37ec0
Sean C Davis <scdavis41@gmail.com>
2023-05-09 06:41:10 -0400

    Style effects for clicking

--- content/utils/get-images.ts
+++ content/utils/get-images.ts
@@ -7,21 +7,73 @@ export type CloudinaryImage = {
   thumbUrl: string;
 };
 
-export async function getImages(): Promise<CloudinaryImage[]> {
-  const cloudinaryImages = await cloudinary.api.resources({
+type CloudinaryImagePage = {
+  /** Page number to use in the UI */
+  pageNumber: number;
+  /** Array of images on the current page */
+  images: CloudinaryImage[];
+};
+
+const PAGE_SIZE = 4;
+
+function transformImage(image: any): CloudinaryImage {
+  return {
+    publicId: image.public_id,
+    thumbUrl: cloudinary.url(image.public_id, {
+      transformation: "stackbit-modal-thumb",
+      secure: true,
+    }),
+  };
+}
+
+export async function getPaginatedImages(
+  pages: CloudinaryImagePage[] = [],
+  next_cursor?: string
+): Promise<CloudinaryImagePage[]> {
+  const response = await cloudinary.api.resources({
     resource_type: "image",
-    max_results: 4,
+    max_results: PAGE_SIZE,
+    next_cursor,
   });
 
-  const images = cloudinaryImages.resources.map((image) => {
-    return {
-      publicId: image.public_id,
-      thumbUrl: cloudinary.url(image.public_id, {
-        transformation: "stackbit-modal-thumb",
-        secure: true,
-      }),
-    };
-  }) as CloudinaryImage[];
+  pages.push({
+    images: response.resources.map(transformImage),
+    pageNumber: pages.length + 1,
+  });
 
-  return images;
+  if (response.next_cursor) {
+    return getPaginatedImages(pages, response.next_cursor);
+  }
+
+  return pages;
 }
+
+// async function getPaginatedImages(): Promise<CloudinaryImagePage[]> {
+// const pages: CloudinaryImagePage[] = [];
+
+//   let response = await getImagePage();
+
+//   // Run again if next_cursor. Otherwise, return the pages.
+//   if (response.next_cursor) {
+//     return getImagePage(pages, response.next_cursor);
+//   }
+
+//   return pages;
+
+//   // return cloudinaryImages.map(transformImage);
+// }
+
+// async function paginateImages(): Promise<CloudinaryImagePage[]> {
+//   return await getImagePage([]);
+
+// }
+
+// export async function getImages(): Promise<CloudinaryImage[]> {
+//   const paginatedImages = await getPaginatedImages([]);
+
+//   // console.log(cloudinaryImages);
+//   // const images = cloudinaryImages.resources.map(
+//   //   transformImage
+//   // ) as CloudinaryImage[];
+//   // return images;
+// }
