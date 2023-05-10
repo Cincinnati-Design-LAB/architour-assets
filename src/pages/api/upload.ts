f281ded800c841ae289c31ef93c4d9ec0c4bb79f
Sean C Davis <scdavis41@gmail.com>
2023-05-09 20:14:30 -0400

    Can upload images to cloudinary

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,25 +1,56 @@
+import { cloudinary } from "@content/utils/client";
 import type { APIRoute } from "astro";
+
+// async function blobToBase64(blob): Promise<string | ArrayBuffer> {
+//   return new Promise((resolve, _) => {
+//     const reader = new FileReader();
+//     reader.onloadend = () => resolve(reader.result);
+//     reader.readAsDataURL(blob);
+//   });
+// }
 
 export const post: APIRoute = async ({ request }) => {
   // const url = new URL(request.url);
   // const params = new URLSearchParams(url.search);
 
-  console.log({ request });
+  // console.log({ request });
+  // console.log(request.body);
+  const body = await request.formData();
+  const image = body.get("imageUrl");
 
-  // const id = params.id;
-  // const product = await getProduct(id);
+  if (!image) {
+    console.error("No image");
+    return new Response(null, {
+      status: 400,
+      statusText: "Bad request",
+    });
+  }
 
-  // if (!product) {
-  //   return new Response(null, {
-  //     status: 404,
-  //     statusText: 'Not found'
-  //   });
-  // }
+  if (typeof image !== "string") {
+    console.error("Image is not a string");
+    return new Response(null, {
+      status: 400,
+      statusText: "Bad request",
+    });
+  }
 
-  // const randomNumber = Math.random() * 100;
+  // console.log({ image });
 
-  // const response = { randomNumber, q: params.get("q") };
-  const response = { request };
+  // const buffer = Buffer
+
+  // console.log(image.toString("base64"));
+  // const buffer = Buffer.from( await image.arrayBuffer())
+
+  // console.log(buffer, buffer.toString("base64"));
+
+  // const base64Image = await blobToBase64(image);
+  const cloudinaryResponse = await cloudinary.uploader.upload(image, {
+    use_filename: true,
+  });
+
+  // console.log(cloudinaryResponse);
+
+  const response = { success: true, public_id: cloudinaryResponse.public_id };
 
   return new Response(JSON.stringify(response), {
     status: 200,
