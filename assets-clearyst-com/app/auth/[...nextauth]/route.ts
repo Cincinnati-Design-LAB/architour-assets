7400b844b75310f051c67ecc1f7b6d09b1f08a2f
Sean C Davis <scdavis41@gmail.com>
2023-06-04 14:26:01 -0400

    Get auth working with Slack

---
+++ assets-clearyst-com/app/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import SlackProvider from "next-auth/providers/slack";
+
+export const authOptions = {
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    SlackProvider({
+      clientId: process.env.SLACK_CLIENT_ID!,
+      clientSecret: process.env.SLACK_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
