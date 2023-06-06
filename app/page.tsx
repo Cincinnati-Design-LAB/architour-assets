389ef7c2d8a142bef0b6479922ae1318ddc35713
Sean C Davis <scdavis41@gmail.com>
2023-06-06 10:37:40 -0400

    Remove auth

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,7 @@ export default function Home() {
     <main>
       <div className="max-w-4xl py-16 mx-auto text-center">
         <h1 className="mb-8 text-4xl font-black">Clearyst Assets</h1>
-        <p>
-          <a
-            href="/auth/signin"
-            className="inline-block px-6 py-2 font-medium text-white bg-blue-500 rounded-sm"
-          >
-            Sign In
-          </a>
-        </p>
+        <p>Nothing to see here.</p>
       </div>
     </main>
   );
