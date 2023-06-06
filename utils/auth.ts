f5d6406798231287feb37c6f5587b6e3fd38f3c0
Sean C Davis <scdavis41@gmail.com>
2023-06-06 09:32:01 -0400

    Try opening access to cookies

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,7 +1,36 @@
+import { AuthOptions } from 'next-auth';
 import SlackProvider from 'next-auth/providers/slack';
 
-export const authOptions = {
-  secret: process.env.AUTH_SECRET,
+export const authOptions: AuthOptions = {
+  secret: process.env.AUTH_SECRET!,
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        path: '/',
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      },
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        path: '/',
+        sameSite: 'none',
+        secure: true,
+      },
+    },
+    csrfToken: {
+      name: `__Host-next-auth.csrf-token`,
+      options: {
+        path: '/',
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      },
+    },
+  },
   providers: [
     SlackProvider({
       clientId: process.env.SLACK_CLIENT_ID!,
